9544759d4e7b036298df2d6afcfd1b565d7f6085
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-02-17 17:28:54 +0200

    security-package/issues/105: ContactFormObserverTest added.

--- ReCaptchaContact/Test/Integration/Observer/ContactFormObserverTest.php
+++ ReCaptchaContact/Test/Integration/Observer/ContactFormObserverTest.php
@@ -13,23 +13,21 @@ use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Data\Form\FormKey;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\View\Element\Message\InterpretationStrategyInterface;
 use Magento\Framework\Message\Manager as MessageManager;
 use Magento\ReCaptcha\Model\CaptchaValidator;
 use Magento\ReCaptchaApi\Api\CaptchaValidatorInterface;
-use Magento\ReCaptchaFrontendUi\Model\CaptchaConfig;
 use Magento\Store\Model\ScopeInterface;
-use Magento\TestFramework\Bootstrap;
 use PHPUnit\Framework\MockObject\MockObject;
 use Magento\TestFramework\TestCase\AbstractController;
+use Magento\TestFramework\Bootstrap;
+
 
 /**
- *
+ * Test for \Magento\ReCaptchaContact\Observer\ContactFormObserver class.
  */
 class ContactFormObserverTest extends AbstractController
 {
-
     /**
      * @var FormKey
      */
@@ -44,7 +42,6 @@ class ContactFormObserverTest extends AbstractController
      * @var CaptchaValidatorInterface|MockObject
      */
     private $captchaValidatorMock;
-
 
     /**
      * @var MutableScopeConfigInterface
@@ -76,15 +73,27 @@ class ContactFormObserverTest extends AbstractController
         $this->_objectManager->addSharedInstance($this->captchaValidatorMock, CaptchaValidator::class);
     }
 
+    /**
+     * Test for Recaptcha is Disabled
+     **/
+    public function testReCaptchaNotConfigured()
+    {
+        $this->sendContactPostAction(false, false);
+
+        $this->assertSessionMessages(
+            $this->contains(
+                "Thanks for contacting us with your comments and questions. We&#039;ll respond to you very soon."
+            ),
+            \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS
+        );
+    }
 
     /**
      * Test for Recaptcha is Disabled
-     *
-     * @magentoConfigFixture recaptcha/frontend/enabled_for_contact 0
-     */
+     **/
     public function testReCaptchaDisabled()
     {
-        $this->sendContactPostAction();
+        $this->sendContactPostAction(true, false);
 
         $this->assertSessionMessages(
             $this->contains(
@@ -96,17 +105,11 @@ class ContactFormObserverTest extends AbstractController
 
     /**
      * Test for Recaptcha is Enabled
-     * Exist access keys
-     *
-     * @magentoAdminConfigFixture  recaptcha/frontend/enabled_for_contact  1
      */
     public function testCorrectRecaptcha()
     {
-        $this->settingsConfiguration->setValue('recaptcha/frontend/public_key', 'test_public_key', ScopeInterface::SCOPE_WEBSITES);
-        $this->settingsConfiguration->setValue('recaptcha/frontend/private_key', 'test_private_key', ScopeInterface::SCOPE_WEBSITES);
-
         $this->captchaValidatorMock->expects($this->once())->method('validate')->willReturn(true);
-        $this->sendContactPostAction();
+        $this->sendContactPostAction(true, true);
 
         $this->assertSessionMessages(
             $this->contains(
@@ -117,19 +120,13 @@ class ContactFormObserverTest extends AbstractController
     }
 
     /**
-     * Test for Recaptcha is Enabled
-     * Exist access keys
      * Test for Incorrect captcha
-     *
-     * @magentoAdminConfigFixture recaptcha/frontend/enabled_for_contact  1
      */
     public function testIncorrectRecaptcha()
     {
-        $this->settingsConfiguration->setValue('recaptcha/frontend/public_key', 'test_public_key', ScopeInterface::SCOPE_WEBSITES);
-        $this->settingsConfiguration->setValue('recaptcha/frontend/private_key', 'test_private_key', ScopeInterface::SCOPE_WEBSITES);
-
         $this->captchaValidatorMock->expects($this->once())->method('validate')->willReturn(false);
-        $this->sendContactPostAction();
+
+        $this->sendContactPostAction(true, true);
 
         /** @var $messages \Magento\Framework\Message\AbstractMessage[] */
         $messages = $this->messageManager->getMessages()->getItems();
@@ -145,22 +142,33 @@ class ContactFormObserverTest extends AbstractController
 
     /**
      * Send Contact form
-     */
-    protected function sendContactPostAction()
+     **/
+    private function sendContactPostAction(bool $captchaIsEnabled = true, bool $captchaIsEnabledForContact = true)
     {
+        if ($captchaIsEnabled) {
+            $this->settingsConfiguration->setValue('recaptcha/frontend/enabled_for_contact', (int)$captchaIsEnabledForContact, ScopeInterface::SCOPE_WEBSITES);
+            $this->settingsConfiguration->setValue('recaptcha/frontend/public_key', 'test_public_key', ScopeInterface::SCOPE_WEBSITES);
+            $this->settingsConfiguration->setValue('recaptcha/frontend/private_key', 'test_private_key', ScopeInterface::SCOPE_WEBSITES);
+        }
+
         $params = [
             'name' => 'customer name',
             'comment' => 'comment',
             'email' => 'user@example.com',
             'hideit' => '',
-            'form_key' => $this->formKey->getFormKey(),
-            'g-recaptcha-response' => 'test_response'
+            'form_key' => $this->formKey->getFormKey()
         ];
+
+        if ($captchaIsEnabled && $captchaIsEnabledForContact) {
+            $params['g-recaptcha-response'] = 'test_response';
+        }
 
         $this->getRequest()->setPostValue($params)->setMethod(HttpRequest::METHOD_POST);
         $this->dispatch('contact/index/post');
 
         $this->assertRedirect($this->stringContains('contact/index'));
+
+        $this->settingsConfiguration->reinit();
     }
 
 }
