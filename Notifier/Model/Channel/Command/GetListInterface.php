573109ba87a86bd4264d7bc74f5da098c60cc51a
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-02-18 16:51:58 +0200

    security-package/issues/53: Check static test.

--- Notifier/Model/Channel/Command/GetListInterface.php
+++ Notifier/Model/Channel/Command/GetListInterface.php
@@ -25,6 +25,7 @@ interface GetListInterface
 {
     /**
      * Find Channel by given SearchCriteria
+     *
      * SearchCriteria is not required because load all sources is useful case
      *
      * @param SearchCriteriaInterface|null $searchCriteria
