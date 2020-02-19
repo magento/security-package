ddef0888ed0f6a700d56beeea85f61e17a3e5cfa
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-02-19 15:40:14 +0200

    security-package/issues/54: Check static test.

--- ReCaptchaApi/Api/CaptchaConfigInterface.php
+++ ReCaptchaApi/Api/CaptchaConfigInterface.php
@@ -19,70 +19,84 @@ interface CaptchaConfigInterface
 {
     /**
      * Get google recaptcha public key
+     *
      * @return string
      */
     public function getPublicKey(): string;
 
     /**
      * Get google recaptcha private key
+     *
      * @return string
      */
     public function getPrivateKey(): string;
 
     /**
      * Sugar method. Return true if captcha keys (public and private) are configured
+     *
      * @return bool
      */
     public function areKeysConfigured(): bool;
 
     /**
      * Get reCaptcha type
+     *
      * @return string
      */
     public function getCaptchaType(): string;
 
     /**
+     * Is Invisible Recaptcha
+     *
      * @return bool
      */
     public function isInvisibleRecaptcha(): bool;
 
     /**
      * Get size
+     *
      * @return string
      */
     public function getSize(): string;
 
     /**
      * Get theme
+     *
      * @return string
      */
     public function getTheme(): ?string;
 
     /**
      * Get minimum frontend score
+     *
      * @return float
      */
     public function getScoreThreshold(): float;
 
     /**
      * Get error message
+     *
      * @return Phrase
      */
     public function getErrorMessage(): Phrase;
 
     /**
      * Get language code
+     *
      * @return string
      */
     public function getLanguageCode(): string;
 
     /**
      * Get position
+     *
      * @return string
      */
     public function getPosition(): ?string;
 
     /**
+     * Is Captcha Enabled
+     *
      * @param string $key
      * @return bool
      */
