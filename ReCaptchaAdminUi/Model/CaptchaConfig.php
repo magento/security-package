bcde217fa057ec04ef85e745108c70f950b3a168
Valerii Naida <vnayda@adobe.com>
2020-02-25 23:59:34 -0600

    security-package/issues/123: reCAPTCHA adapters modularity

---
+++ ReCaptchaAdminUi/Model/CaptchaConfig.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ReCaptchaAdminUi\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Phrase;
+use Magento\ReCaptchaApi\Api\CaptchaConfigInterface;
+
+/**
+ * @inheritdoc
+ */
+class CaptchaConfig implements CaptchaConfigInterface
+{
+    private const XML_PATH_PUBLIC_KEY = 'recaptcha/backend/public_key';
+    private const XML_PATH_PRIVATE_KEY = 'recaptcha/backend/private_key';
+
+    private const XML_PATH_SCORE_THRESHOLD = 'recaptcha/backend/score_threshold';
+    private const XML_PATH_SIZE = 'recaptcha/backend/size';
+    private const XML_PATH_THEME = 'recaptcha/backend/theme';
+    private const XML_PATH_POSITION = 'recaptcha/frontend/position';
+    private const XML_PATH_LANGUAGE_CODE = 'recaptcha/frontend/lang';
+
+    private const XML_PATH_TYPE_FOR = 'recaptcha_backend/type_for/';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var array
+     */
+    private $captchaErrorMessages;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param array $captchaErrorMessages
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        array $captchaErrorMessages = []
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->captchaErrorMessages = $captchaErrorMessages;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPublicKey(): string
+    {
+        return trim((string)$this->scopeConfig->getValue(self::XML_PATH_PUBLIC_KEY));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPrivateKey(): string
+    {
+        return trim((string)$this->scopeConfig->getValue(self::XML_PATH_PRIVATE_KEY));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCaptchaType(): string
+    {
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_TYPE);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSize(): string
+    {
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_SIZE);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTheme(): string
+    {
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_THEME);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getScoreThreshold(): float
+    {
+        return min(1.0, max(0.1, (float) $this->scopeConfig->getValue(
+            self::XML_PATH_SCORE_THRESHOLD
+        )));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getErrorMessage(): Phrase
+    {
+        foreach ($this->captchaErrorMessages as $captchaErrorMessage) {
+            if ($this->getCaptchaType() === $captchaErrorMessage['type']) {
+                return __($captchaErrorMessage['message']);
+            }
+        }
+
+        return __('Incorrect ReCaptcha validation');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getLanguageCode(): string
+    {
+        return (string)$this->scopeConfig->getValue(
+            self::XML_PATH_LANGUAGE_CODE
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getInvisibleBadgePosition(): string
+    {
+        return (string)$this->scopeConfig->getValue(
+            self::XML_PATH_POSITION
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isCaptchaEnabledFor(string $key): bool
+    {
+        return $this->areKeysConfigured() && (null !== $this->getCaptchaTypeFor($key));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCaptchaTypeFor(string $key): ?string
+    {
+        $type = $this->scopeConfig->getValue(
+            self::XML_PATH_TYPE_FOR . $key
+        );
+        return $type;
+    }
+
+    /**
+     * Return true if reCAPTCHA keys (public and private) are configured
+     *
+     * @return bool
+     */
+    private function areKeysConfigured(): bool
+    {
+        return $this->getPrivateKey() && $this->getPublicKey();
+    }
+}
