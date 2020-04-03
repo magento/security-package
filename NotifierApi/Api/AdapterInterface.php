369ca8157067f8c554f81942b2c313782918ce58
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-04-03 16:12:32 +0300

    security-package/issues/180: Fix static test.

--- NotifierApi/Api/AdapterInterface.php
+++ NotifierApi/Api/AdapterInterface.php
@@ -8,8 +8,6 @@ declare(strict_types=1);
 
 namespace Magento\NotifierApi\Api;
 
-use Magento\NotifierApi\Model\AdapterEngine\AdapterEngineInterface;
-
 /**
  * Interface AdapterInterface
  * @api
@@ -21,13 +19,6 @@ interface AdapterInterface
      * @return string
      */
     public function getCode(): string;
-
-    /**
-     * Get Engine
-     *
-     * @return AdapterEngineInterface
-     */
-    public function getEngine(): AdapterEngineInterface;
 
     /**
      * Get adapter description
