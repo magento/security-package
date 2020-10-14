8c678be946a0bcebd05080232941112e4e821767
ogorkun <ogorkun@adobe.com>
2020-10-14 14:42:36 -0500

    MC-37782: Introduce mechanism that allows ReCaptcha validation for web APIs

---
+++ ReCaptchaWebapiUi/view/frontend/web/js/webapiReCaptcha.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/* global grecaptcha */
+define(
+    [
+        'Magento_ReCaptchaFrontendUi/js/reCaptcha',
+        'Magento_ReCaptchaWebapiUi/js/webapiReCaptchaRegistry',
+    ],
+    function (Component, registry) {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                autoTrigger: false
+            },
+
+            /**
+             * Provide the token to the registry.
+             *
+             * @param {String} token
+             */
+            reCaptchaCallback: function (token) {
+                //Make the token retrievable in other UI components.
+                registry.tokens[this.getReCaptchaId()] = token;
+                if ((typeof registry._listeners[this.getReCaptchaId()]) !== "undefined") {
+                    registry._listeners[this.getReCaptchaId()](token);
+                }
+            },
+
+            /**
+             * Register this ReCaptcha.
+             *
+             * @param {Object} parentForm
+             * @param {String} widgetId
+             */
+            initParentForm: function (parentForm, widgetId) {
+                var trigger = function () {
+                    grecaptcha.execute(widgetId);
+                };
+                if (this.autoTrigger) {
+                    //Validate ReCaptcha when initiated
+                    trigger();
+                    registry.triggers[this.getReCaptchaId()] = new Function();
+                } else {
+                    registry.triggers[this.getReCaptchaId()] = trigger;
+                }
+                this.tokenField = null;
+            }
+        });
+    }
+);
---
+++ ReCaptchaWebapiUi/view/frontend/web/js/webapiReCaptchaRegistry.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([], function () {
+    'use strict';
+
+    return {
+        /**
+         * recaptchaId: token map.
+         *
+         * Tokens for already verified recaptcha.
+         */
+        tokens: {},
+
+        /**
+         * recaptchaId: triggerFn map.
+         *
+         * Call a trigger to initiate a recaptcha verification.
+         */
+        triggers: {},
+
+        /**
+         * recaptchaId: callback map
+         */
+        _listeners: {},
+
+        /**
+         * Add a listener to when the ReCaptcha finishes verification.
+         *
+         * @param id ReCaptchaId
+         * @param func Will be called back with the token.
+         */
+        addListener: function (id, func) {
+            if (this.tokens.hasOwnProperty(id)) {
+                func(this.tokens[id]);
+            } else {
+                this._listeners[id] = func;
+            }
+        }
+    };
+});
