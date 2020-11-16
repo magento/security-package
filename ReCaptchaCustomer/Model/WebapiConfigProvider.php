6fc5065d461f9fbe229e34f9e282ba99292f1516
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2020-11-16 14:14:14 +0200

    MC-37786: Cover password reset/change web APIs with ReCaptcha

---
+++ ReCaptchaCustomer/Model/WebapiConfigProvider.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ReCaptchaCustomer\Model;
+
+use Magento\ReCaptchaUi\Model\IsCaptchaEnabledInterface;
+use Magento\ReCaptchaUi\Model\ValidationConfigResolverInterface;
+use Magento\ReCaptchaValidationApi\Api\Data\ValidationConfigInterface;
+use Magento\ReCaptchaWebapiApi\Api\Data\EndpointInterface;
+use Magento\ReCaptchaWebapiApi\Api\WebapiValidationConfigProviderInterface;
+
+/**
+ * Provide checkout related endpoint configuration.
+ */
+class WebapiConfigProvider implements WebapiValidationConfigProviderInterface
+{
+    private const RESET_PASSWORD_CAPTCHA_ID = 'customer_forgot_password';
+
+    private const CHANGE_PASSWORD_CAPTCHA_ID = 'customer_edit';
+
+    /**
+     * @var IsCaptchaEnabledInterface
+     */
+    private $isEnabled;
+
+    /**
+     * @var ValidationConfigResolverInterface
+     */
+    private $configResolver;
+
+    /**
+     * @param IsCaptchaEnabledInterface $isEnabled
+     * @param ValidationConfigResolverInterface $configResolver
+     */
+    public function __construct(IsCaptchaEnabledInterface $isEnabled, ValidationConfigResolverInterface $configResolver)
+    {
+        $this->isEnabled = $isEnabled;
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getConfigFor(EndpointInterface $endpoint): ?ValidationConfigInterface
+    {
+        //phpcs:disable Magento2.PHP.LiteralNamespaces
+        if ($endpoint->getServiceMethod() === 'resetPassword'
+            || $endpoint->getServiceMethod() === 'initiatePasswordReset'
+            || $endpoint->getServiceClass() === 'Magento\CustomerGraphQl\Model\Resolver\ResetPassword') {
+            if ($this->isEnabled->isCaptchaEnabledFor(self::RESET_PASSWORD_CAPTCHA_ID)) {
+                return $this->configResolver->get(self::RESET_PASSWORD_CAPTCHA_ID);
+            }
+        }
+        if ($endpoint->getServiceMethod() === 'changePasswordById'
+            || $endpoint->getServiceClass() === 'Magento\CustomerGraphQl\Model\Resolver\ChangePassword') {
+            if ($this->isEnabled->isCaptchaEnabledFor(self::CHANGE_PASSWORD_CAPTCHA_ID)) {
+                return $this->configResolver->get(self::CHANGE_PASSWORD_CAPTCHA_ID);
+            }
+        }
+
+        //phpcs:enable Magento2.PHP.LiteralNamespaces
+
+        return null;
+    }
+}
