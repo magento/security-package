25d28a8c4fbcca03b7d8e18f1b741502eea5ddd7
Rizwan Khan <rizwan.khan@BLR1-LMC-N90394.local>
2024-05-20 14:55:32 +0530

    AC-9797: 2FA functionality enhancement

--- TwoFactorAuth/Block/Provider/Google/Auth.php
+++ TwoFactorAuth/Block/Provider/Google/Auth.php
@@ -8,37 +8,12 @@ declare(strict_types=1);
 namespace Magento\TwoFactorAuth\Block\Provider\Google;
 
 use Magento\Backend\Block\Template;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 
 /**
  * @api
  */
 class Auth extends Template
 {
-    /**
-     * Config path for the 2FA Attempts
-     */
-    public const XML_PATH_2FA_RETRY_ATTEMPTS = 'twofactorauth/general/twofactorauth_retry';
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    /**
-     * @param \Magento\Backend\Block\Template\Context $context
-     * @param ScopeConfigInterface $scopeConfig
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Backend\Block\Template\Context $context,
-        ScopeConfigInterface $scopeConfig,
-        array $data = []
-    ) {
-        parent::__construct($context, $data);
-        $this->scopeConfig = $scopeConfig;
-    }
-
     /**
      * @inheritdoc
      */
@@ -49,9 +24,6 @@ class Auth extends Template
 
         $this->jsLayout['components']['tfa-auth']['successUrl'] =
             $this->getUrl($this->_urlBuilder->getStartupPageUrl());
-
-        $this->jsLayout['components']['tfa-auth']['attempts'] =
-            $this->scopeConfig->getValue(self::XML_PATH_2FA_RETRY_ATTEMPTS);
 
         return parent::getJsLayout();
     }
